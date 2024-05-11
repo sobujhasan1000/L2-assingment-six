2fc538c6988fa1679a285cda94e19dfca6578761
unknown <sobujhasan1000@gmail.com>
2024-05-11 23:56:51 +0600

    tale is done

--- src/pages/dashboard/DonationsTabile.tsx
+++ src/pages/dashboard/DonationsTabile.tsx
@@ -1,39 +1,40 @@
+import { useGetDonationQuery } from "../../redux/api/api";
+
+type TDonation = {
+  title: string;
+  image: string;
+  category: string;
+  amount: number;
+  // Add more fields as needed
+};
 const DonationsTabile = () => {
+  const { data, isLoading, error } = useGetDonationQuery({});
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+  if (!data) return null;
+  console.log(data, "find my data");
+
   return (
     <div>
       <div className="overflow-x-auto">
         <table className="table">
-          {/* head */}
           <thead>
             <tr>
               <th></th>
-              <th>Name</th>
-              <th>Job</th>
-              <th>Favorite Color</th>
+              <th>title</th>
+              <th>category</th>
+              <th>amount</th>
             </tr>
           </thead>
           <tbody>
-            {/* row 1 */}
-            <tr>
-              <th>1</th>
-              <td>Cy Ganderton</td>
-              <td>Quality Control Specialist</td>
-              <td>Blue</td>
-            </tr>
-            {/* row 2 */}
-            <tr className="hover">
-              <th>2</th>
-              <td>Hart Hagerty</td>
-              <td>Desktop Support Technician</td>
-              <td>Purple</td>
-            </tr>
-            {/* row 3 */}
-            <tr>
-              <th>3</th>
-              <td>Brice Swyre</td>
-              <td>Tax Accountant</td>
-              <td>Red</td>
-            </tr>
+            {data.donations.map((item: TDonation) => (
+              <tr key={item.title}>
+                <th></th>
+                <td>{item.title}</td>
+                <td>{item.category}</td>
+                <td>{item.amount}</td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
